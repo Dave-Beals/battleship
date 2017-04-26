c5f3dd2e42a1337c1f1ac37de13c86da9035249f
Dave <dbeals85@gmail.com>
2017-04-25 19:20:07 -0700

    another tardy commit of js file with completed code but no qa yet

---
+++ battleship.js
@@ -0,0 +1,36 @@
+var location1 = 3;
+var location2 = 4;
+var location3 = 5;
+var guess;
+// guess variable is blank until user inputs a guess
+var hits = 0;
+var guesses = 0;
+var isSung = false;
+
+while (isSunk == false) {
+    guess = prompt("Ready, aim, fire! (enter a number from 0-6) :");
+    if (guess < 0 || guess > 6) {
+      alert("Please enter a valid cell number!");
+    } else {
+        guesses = guesses + 1;
+
+        if (guess == location1 || guess == location2 || guess == location3) {
+            hits = hits + 1;
+    // more complex version of this statement below. Using better version, combined w/ boolean operator)
+    // } else if (guess == location2) {
+    //   hits = hits + 1;
+    // } else if (guess == location3) {
+    //   hits = hits + 1;
+    // }
+          if (hits == 3) {
+              isSunk = true;
+              alert("You sank my battleship!");
+          }
+        } else {
+          alert("MISS");
+        }
+    }
+  }
+var stats = "You took " + guesses + " guesses to sink the battleship, " +
+              "which means your shooting accuracy was " + (3/guesses);
+alert(stats);
